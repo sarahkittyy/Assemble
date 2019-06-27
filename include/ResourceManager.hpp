fda31f6d3c0f7ff17abd428fb01dd9b5198cbe94
sarahkittyy <sarahohlin1@gmail.com>
2019-06-26 22:50:37 -0400

    added json to resource manager

--- include/ResourceManager.hpp
+++ include/ResourceManager.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <fstream>
+#include <stdexcept>
 #include <string>
 #include <unordered_map>
+#include "nlohmann/json.hpp"
 
 /**
  * @brief Manages resources such that only one instance
@@ -20,7 +23,17 @@ public:
 	 */
 	sf::Texture* texture(std::string path);
 
+	/**
+	 * @brief Get the json data of a given file.
+	 * 
+	 * @param path The path to the file.
+	 * @return nlohmann::json* The json data inside that file.
+	 */
+	nlohmann::json* json(std::string path);
+
 private:
 	/// Mapping of paths to the textures at that path.
 	std::unordered_map<std::string, sf::Texture> mTextures;
+	/// Mapping of paths to the json data inside those files.
+	std::unordered_map<std::string, nlohmann::json> mJSON;
 };
--- src/ResourceManager.cpp
+++ src/ResourceManager.cpp
@@ -11,3 +11,22 @@ sf::Texture* ResourceManager::texture(std::string path)
 
 	return &mTextures[path];
 }
+
+nlohmann::json* ResourceManager::json(std::string path)
+{
+	// Try to find the json
+	auto found = mJSON.find(path);
+	if (found == mJSON.end())
+	{
+		// Load the json from the file.
+		std::ifstream file(path);
+		if (!file)
+		{
+			throw std::runtime_error("Could not open file " + path);
+		}
+		file >> mJSON[path];
+		file.close();
+	}
+
+	return &mJSON[path];
+}
